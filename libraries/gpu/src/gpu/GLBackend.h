22d279b93906036d6f630de1b8224d6ad48e4250
dmt <dmt@completelydifferent.com>
2016-02-15 14:21:17 -0800

    add glLineWidth to state
    add glLineWidth to gpu backend state

--- libraries/gpu/src/gpu/GLBackend.h
+++ libraries/gpu/src/gpu/GLBackend.h
@@ -228,6 +228,8 @@ public:
 
     void do_setStateColorWriteMask(uint32 mask);
 
+	void do_setStateLineWidth(float lineWidth);
+
     // Repporting stats of the context
     class Stats {
     public:
